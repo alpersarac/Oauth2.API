551f2594a76f713b2ec0cc914a18fb6d81642328
SARAC <careeralpersarac@gmail.com>
2024-05-14 08:43:41 +0300

    AppUserRole

--- Oauth2.Identity/ApplicationIdentityDbContext.cs
+++ Oauth2.Identity/ApplicationIdentityDbContext.cs
@@ -9,7 +9,9 @@ using System.Threading.Tasks;
 
 namespace Oauth2.Identity
 {
-    public sealed class ApplicationIdentityDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    public sealed class ApplicationIdentityDbContext : IdentityDbContext<AppUser,
+        AppRole, 
+        Guid,IdentityUserClaim<Guid>, AppUserRole, IdentityUserLogin<Guid>,IdentityRoleClaim<Guid>,IdentityUserToken<Guid>>
     {
         public ApplicationIdentityDbContext(DbContextOptions options) : base(options)
         {
